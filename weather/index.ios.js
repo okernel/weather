bd76825b562029c5842ab6239cc443b7613c7664
OKernel <liuair@programmer.net>
2016-01-18 01:46:36 +0800

    set one pin to the mapview

--- weather/index.ios.js
+++ weather/index.ios.js
@@ -1,53 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
+
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    MapView,
+    Text,
+    View,
+    StyleSheet
 } = React;
 
-var weather = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+var Weather = React.createClass({
+
+    getInitialState: function() {
+        return {
+            pin: {
+                latitude: 0,
+                longitude: 0
+            }
+        };
+    },
+    render:function() {
+
+        return <MapView
+            annotations = {[this.state.pin]}
+            onRegionChangeComplete = {this.onRegionChangeComplete}
+            style = {styles.map}>
+
+        </MapView>
+    },
+    onRegionChangeComplete: function(region) {
+
+        this.setState({
+            pin: {
+                longitude: region.longitude,
+                latitude: region.latitude
+            }
+        });
+    }
+
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
+    map: {
+        flex: 1
+    }
+
 });
 
-AppRegistry.registerComponent('weather', () => weather);
+AppRegistry.registerComponent('weather',()=> Weather);
